90d38fbc2b2c1315b0efca92239dc30aeeb4f72c
Fábio <fabiorochadacosta@gmail.com>
2015-10-30 23:29:43 -0200

    Tentativa appharbor

--- Pipocao/Global.asax.cs
+++ Pipocao/Global.asax.cs
@@ -26,19 +26,6 @@ namespace Pipocao
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
-
-            var uriString = ConfigurationManager.AppSettings["SQLSERVER_URI"];
-            var uri = new Uri(uriString);
-            var connectionString = new SqlConnectionStringBuilder
-            {
-                DataSource = uri.Host,
-                InitialCatalog = uri.AbsolutePath.Trim('/'),
-                UserID = uri.UserInfo.Split(':').First(),
-                Password = uri.UserInfo.Split(':').Last(),
-            }.ConnectionString;
-
-            var DatabaseContext = new DatabaseContext();
-            DatabaseContext.Database.Connection.ConnectionString = connectionString;
         }
     }
 }
